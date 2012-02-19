bc4e6f1a0f6905437bd36be8fbc473c7df949cd8
Ben Gustafson <bengus28@gmail.com>
2012-02-19 16:17:44 -0600

    Shell of Dogson

--- src/Dodgson.java
+++ src/Dodgson.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+
 
 public class Dodgson {
-	
 
+	ArrayList<String> searchedTerms;
+	
+public Dodgson ()
+{
+	searchedTerms = null;
+}
+public ArrayList<String> find (String searchTerm, String endTerm, ArrayList<String> tree)
+{
+	return tree;
+}
 }
