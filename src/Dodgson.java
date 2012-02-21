e022040b50aeb935cf178394208fd14eb7e3b151
Adam Bulgatz <adam@bulgatz.com>
2012-02-21 16:55:57 -0600

    Added currently broken method to find path using a HashMap

--- src/Dodgson.java
+++ src/Dodgson.java
@@ -1,15 +1,21 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Dodgson {
 
 	private final int FROM = 0, TO = 1;
 	
+	private final String RELATED = "related", BREADCRUMBS = "breadcrumbs";
+	
+	Map<String, Map<String, List<String>>> searchTree = new HashMap<String, Map<String, List<String>>>();
+	
 	public Dodgson() {
 		
 	}
 	
-	public ArrayList<String> findPath(String[] wordPair, Lexicon lexicon) {
+	public List<String> findPath(String[] wordPair, Lexicon lexicon) {
 		String startWord = wordPair[FROM];
 		String endWord = wordPair[TO];
 		
@@ -36,6 +42,24 @@ public class Dodgson {
 			System.out.println(printOutput);
 		// End Print
 		
+		searchTree.put(startWord, new HashMap<String, List<String>>());
+		searchTree.get(startWord).put(RELATED, lexicon.wordsOneOff(startWord));
+		searchTree.get(startWord).put(BREADCRUMBS, new ArrayList<String>());
+		searchTree.get(startWord).get(BREADCRUMBS).add(startWord);
+		
+		for (String parentWord : searchTree.keySet()) {
+			for (String relatedWord : searchTree.get(parentWord).get(RELATED)) {
+				if (relatedWord.equals(endWord)) {
+					List<String> output = searchTree.get(parentWord).get(BREADCRUMBS);
+					output.add(relatedWord);
+					return output;
+				}
+				searchTree.put(relatedWord, new HashMap<String, List<String>>());
+				searchTree.get(relatedWord).put(RELATED, lexicon.wordsOneOff(relatedWord));
+				searchTree.get(relatedWord).put(BREADCRUMBS, searchTree.get(parentWord).get(BREADCRUMBS));
+				searchTree.get(relatedWord).get(BREADCRUMBS).add(relatedWord);
+			}
+		}
 		return null;
 	}
 }
