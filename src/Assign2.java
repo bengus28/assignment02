958de74c5892c08622d6856f80810cd94f3b9de4
Adam Bulgatz <adam@bulgatz.com>
2012-03-24 22:50:19 -0500

    Removed some test prints

--- src/Assign2.java
+++ src/Assign2.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.util.Arrays;
 import java.util.List;
 
 
@@ -26,13 +25,14 @@ public class Assign2 {
 		
 		
 		// Test some lexicon functionality
-		String[] testWords = {"hello", "hell", "apple", "ap", "can", "candle"};
-		for (String word : testWords) {
-			String output = "\tWord: " + word;
-				output += "\n\t\tisWord(): " + wordList.isWord(word);
-				output += "\n\t\twordsOneOff(): " + wordList.wordsOneOff(word);
-				output += "\n\t\tisPrefix(): " + wordList.isPrefix(word);
-		}
+//		String[] testWords = {"hello", "hell", "apple", "ap", "can", "candle"};
+//		for (String word : testWords) {
+//			String output = "\tWord: " + word;
+//				output += "\n\t\tisWord(): " + wordList.isWord(word);
+//				output += "\n\t\twordsOneOff(): " + wordList.wordsOneOff(word);
+//				output += "\n\t\tisPrefix(): " + wordList.isPrefix(word);
+//			System.out.println(output);
+//		}
 		
 		
 		// Dodgson
@@ -41,19 +41,20 @@ public class Assign2 {
 		// For each word pair
 		for (String[] wordPair : wordPairs) {
 			
-		// Print the word pair
-			
+			// Print the word pair
+			System.out.println("Word pair: " + wordPair[0].toLowerCase() + " È " + wordPair[1].toLowerCase()); 
+		
 			// Find the path
 			List<String> path = dodgson.findPath(wordPair, wordList);
-			if (path != null)
-			{
-				int i=0;
-				while (i < path.size() - 1 )
-				{
-					System.out.print(path.get(i) + ", ");
+			String output = "\tPath: ";
+			if (path != null) {
+				int i = 0;
+				while (i < path.size() - 1) {
+					output += path.get(i).toLowerCase() + " È ";
 					i++;
 				}
-				System.out.println(path.get(i));
+				output += path.get(i).toLowerCase();
+				System.out.println(output);
 			}
 			else
 				System.out.println("\tNo path exists.");
